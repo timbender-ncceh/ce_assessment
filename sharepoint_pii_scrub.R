3463eb6f37dfebb6762d9642b99007092eb42806
Tim Bender <105810134+timbender-ncceh@users.noreply.github.com>
2023-08-09 07:19:52 -0400

    Initial commit

---
+++ sharepoint_pii_scrub.R
@@ -0,0 +1,131 @@
+## WARNING - PLEASE READ ##
+## --------------------- ##
+
+# THIS SCRIPT DELETES FILES PERMANENTLY. THERE IS ALWAYS A NON-ZERO RISK THAT
+# IMPORTANT FILES MAY BE DELETED INADVERTENTLY WHEN A SCRIPT IS WRITTEN FOR THIS
+# PURPOSE.
+
+# DO NOT ASSUME THIS FILE HAS BEEN TESTED OR IS SAFE, OR DOES WHAT YOU THINK
+# IT'S SUPPOSED TO DO. EVEN IF IT USED TO WORK, THINGS CHANGE AND CODE BREAKS.
+# CODE THAT DELETES FILES IS VERY DANGEROUS SO PROCEED WITH CAUTION.  THIS IS
+# BEST PRACTICES STUFF.
+
+# DO NOT RUN THIS SCRIPT IF YOU DO NOT UNDERSTAND WHAT IT DOES.
+
+# ONE LINE OF CODE IS ALL IT TAKES TO DELETE EVERY FILE FROM YOUR HARD DRIVE
+# AND/OR NETWORK.  THAT LINE OF CODE IS USED IN THIS SCRIPT, BUT MODIFIED
+# SLIGHTLY SO THAT INSTEAD OF DELETING EVERYTHING, IT ONLY DELETES FILES
+# MATCHING A SPECIFIC (FILENAME) CRITERIA. FAILSAFES WILL BE IMPLEMENTED TO
+# CHECK FOR COMMON ERRORS AND MISTAKES BUT FAILSAFES ARE NOT TO BE RELIED UPON.
+# THIS IS BEST PRACTICES STUFF
+
+## OVERVIEW - PLEASE READ ##
+## ---------------------- ##
+
+# THIS SCRIPT IS DESIGNED TO SEARCH THROUGH A SPECIFIED FILE DIRECTORY AND ALL
+# SUB-DIRECTORIES THAT IT CONTAINS FOR FILES WITH FILENAMES MATCHING A SPECIFIC
+# VALUE.  WHEN A MATCHING FILE IS FOUND, THE SCRIPT WILL USE THE WINDOWS
+# OPERATING SYSTEM TO FIRST OVERWRITE THE PERSONALLY IDENTIFYING INFORMATION
+# (PII) WITHIN THE FILE WITH NULL VALUES, SAVE THE CHANGES TO THE FILE, THEN
+# PERMANENTLY REMOVE THE FILE FROM THE DIRECTORY.
+
+## POTENTIAL ISSUES ##
+## ---------------- ##
+
+# THE CODE WILL SEARCH FOR ALL FILES NAMED "Client.csv" WHICH IS WHERE WE KNOW
+# PII DATA IS LOCATED IN HMIS EXPORTS.
+
+# ISSUE #1: FALSE POSITIVES: ANY FILES SAVED IN THE DIRECTORY NAMED "Client.csv"
+# WILL BE DELETED WITHOUT CONSIDERATION FOR WHAT THE FILE CONTAINS.
+# THEORETICALLY THERE SHOULD NOT BE ANY FILES WITH THE SAME NAME THAT ARE NOT A
+# PRODUCT OF THE HMIS_CSV EXPORT PROCESS.  HOWEVER, THERE IS ALWAYS A CHANCE
+# THAT SOMEONE HAS UNKNOWINGLY NAMED AN IMPORTANT, UNRELATED FILE "Client.csv"
+# AND THIS SCRIPT WILL PERMANENTLY DELETE IT.
+
+# ISSUE #2: FALSE NEGATIVES: ANYONE COULD CIRCUMVENT THE SECURITY MEASURES
+# AFFORDED BY THIS LOGIC BY SIMPLY RENAMING THE "Client.csv" FILE TO SOME OTHER
+# NAME SUCH AS "new_client.csv". THIS FILE WOULD NOT BE FOUND BY THIS SCRIPT,
+# AND THUS PII WOULD REMAIN ON IN THE DIRECTORY.
+
+# ISSUES #1 AND #2 CAN BE ADDRESSED WITH A REASONABLE LEVEL OF CERTAINITY BY
+# ADDING ADDITIONAL LOGIC TO THIS CODE, BUT WE INTEND THIS TO BE A QUICK
+# IMPLEMENTATION THAT CAN ALSO BE EASY TO READ AND COMPREHEND BY THE AUTHOR AND
+# NON-AUTHORS ALIKE.  COMPLEXITY IS NOT A GOOD THING WHEN IT COMES TO FILE
+# MANAGEMENT.
+
+## ---- END OF OVERVIEW ---- ##
+
+# Libraries----
+library(dplyr)
+library(readr)
+
+rm(list = ls())
+cat('\f')
+gc()
+
+# To Do----
+# 1) Remove .7z files and .zip files from download folder
+
+# Functions----
+
+search.dirs <- c("C:/Users/TimBender/North Carolina Coalition to End Homelessness/PM Data Center - Documents/Reporting", 
+                 "C:/Users/TimBender/Documents/R")
+
+# Variables----
+
+for(search.dir in search.dirs){
+  search.filename <- "Client.csv"
+  pii.colnames    <- c("FirstName",
+                       "MiddleName",
+                       "LastName",
+                       "NameSuffix",
+                       "SSN",
+                       "DOB")
+  
+  # Logic----
+  # convert filename to regex
+  search.filename2 <-  paste("^", search.filename, "$", sep = "", collapse = "") %>%
+    gsub(pattern = "\\.", replacement = "\\\\\\.")
+  
+  # find all instances of file
+  all.files <- list.files(
+    path = search.dir,
+    pattern = search.filename2,
+    recursive = T,
+    full.names = F,
+    include.dirs = T
+  )
+  
+  all.files <-  paste(search.dir, all.files, sep = "/") %>%
+    gsub(pattern = "\\/{2,}", "/", .)
+  
+  # loop through each filename
+  
+  for (i in all.files) {
+    # read file
+    temp <- read_csv(i)
+    
+    # loop through pii columns
+    for (ic in pii.colnames) {
+      # overwrite pii data with NA
+      if (ic %in% colnames(temp)) {
+        temp[, ic] <- NA
+      }
+    }
+    
+    # save as Client.csv to overwrite data
+    write_csv(x      = temp,
+              file   = i,
+              append = F)
+    
+    
+    # delete file(s)
+    # <<< not yet added >>> ----
+    
+    # cleanup
+    rm(temp)
+  }
+}
+
+
+
---
+++ simulate_data.R
@@ -0,0 +1,230 @@
+####################################
+#####.........README...........#####
+####################################
+
+# Latest readme at: 
+#<<< TODO: CREATE URL ON GITHUB>>>
+
+# Generate Simulated Data from HMIS .csv files
+
+#NOTES:
+#
+#1) PII will be overwritten by hashing.  All fields to be hashed must be
+#identified by column_name within variable "pii.cols"
+#
+#2) input and output directories must be set accordingly via input_files_dir and
+#output_files_dir
+#
+
+################################### 
+####......../README...........#####
+####################################
+
+load.libs <- c("data.table",
+               "dplyr",
+               "lubridate",
+               "lexicon",    # common names database
+               "readr",      # data read/write 
+               "zip",        # compress and write data to .zip files
+               "rstudioapi") # get directory info
+
+# check if libraries/packages are installed
+for(l in load.libs){
+  # Is package 'l' installed? If not, ask user for permission to install package
+  my.pkgs <- packageStatus()$inst$Package # gets a list of all installed packages
+  if(!l %in% my.pkgs){
+    cat('\n')
+    # if run outside an interactive session, automatically install missing packages
+    permit.install.pkg <- ifelse(interactive(), readline(prompt = paste("Authorize Script to install package [",l,"] (y/n)?", sep = "")), "y")
+    # If authorized, install package
+    ifelse(test = tolower(permit.install.pkg) %in% c("y", "yes"), 
+           install.packages(l), 
+           print("User declined installation of required package; script will not execute properly until installation is permitted. If you receive this message in error contact Tim Bender"))
+  }else{
+    cat('\n')
+    print(paste(l,"already installed"))
+  }
+  # now that library has been installed if missing, load library
+  library(l, character.only = T)
+  print(paste(l,"loaded"))
+}
+
+rm(l, load.libs, my.pkgs, list=ls()); cat('\f'); gc()
+
+# Vars----
+simulated_sample_size <- 100  # set desired number of rows and/or observations to sample from each file
+input_files_dir  <- "C:/Users/TimBender/Documents/tableau/DHHS_vaccination_linkage/data"
+output_files_dir <- "C:/Users/TimBender/Documents/R/ncceh/projects/codi"
+
+# identify all pii column names: 
+pii.cols             <- c("SSN","UserLastName", "LastName",
+                          "UserFirstName", "MiddleName", "FirstName",
+                          "UserPhone", "UserEmail", "DOB")
+pii.cols <- ""
+lastname.cols        <- c("")
+firstmiddlename.cols <- c("")
+phone.cols           <- c("")
+email.cols           <- c("")
+dob.cols             <- c("")
+
+# Set input file directory location----
+setwd(input_files_dir)
+
+# generate sample data by randomly selecting attributes from each column----
+temp.files <- list.files(pattern = "csv$") 
+new.files <- list()
+n_rows <- simulated_sample_size
+for(i1 in temp.files){
+  try(temp.file <- read_csv(i1))
+  
+  for(i3 in 1:ncol(temp.file)){
+    # hash pii
+    if(names(temp.file[,i3]) %in% pii.cols){
+      try(temp.file[,i3] <- openssl::sha256(x = as.character(unlist(as.vector(temp.file[,i3]))), key = "ncceh"))
+    }
+    # place dummy lastnames
+    if(names(temp.file[,i3]) %in% lastname.cols){
+      #stop("ERROR!!!!!!!!!!!!!!!")
+      try(temp.file[,i3] <- sample(x = lexicon::freq_last_names$Surname, size = nrow(temp.file), replace = T, 
+                               prob = lexicon::freq_last_names$prop))
+    }
+    # place dummy firstnames
+    if(names(temp.file[,i3]) %in% firstmiddlename.cols){
+      try(temp.file[,i3] <- sample(x = lexicon::freq_first_names$Name, size = nrow(temp.file), replace = T, 
+                                   prob = lexicon::freq_first_names$prop))
+    }
+    # format phone number
+    if(names(temp.file[,i3]) %in% phone.cols){
+      try(temp.file[!unname(unlist(is.na(unlist(temp.file[,i3])))),i3] <- 5555555555)
+    }
+    #format email address
+    if(names(temp.file[,i3]) %in% email.cols){
+      try(temp.file[!unname(unlist(is.na(unlist(temp.file[,i3])))),i3] <- "fakename@fakeemail.com")
+    }
+    #format DOB
+    if(names(temp.file[,i3]) %in% dob.cols){
+      try(temp.file[!unname(unlist(is.na(unlist(temp.file[,i3])))),i3] <- as_date(0))
+    }
+  }
+  temp.df   <- NULL
+  if(is.null(temp.df)){
+    for(c1 in 1:ncol(temp.file)){
+      
+      if(is.null(temp.df)){
+        try(temp.df <- temp.file[sample(1:nrow(temp.file), size = min(c(n_rows, nrow(temp.file))), replace = F),c1])
+      }else{
+        try(temp.df <- cbind(temp.df, temp.file[sample(1:nrow(temp.file), size = min(c(n_rows, nrow(temp.file))), replace = F),c1]))
+      }
+    }
+  }
+  try(new.files[[i1]] <-  temp.df)
+  rm(temp.file)
+}
+
+# generate sample data by randomly selecting rows from sample data----
+temp.files2 <- list.files(pattern = "csv$") 
+new.files2 <- list()
+n_rows <- simulated_sample_size
+for(i1 in temp.files2){
+  try(temp.file2 <- read_csv(i1))
+  for(i3 in 1:ncol(temp.file2)){
+    # hash pii
+    if(names(temp.file2[,i3]) %in% pii.cols){
+      try(temp.file2[,i3] <- openssl::sha256(x = as.character(unlist(as.vector(temp.file2[,i3]))), key = "ncceh"))
+    }
+    # place dummy lastnames
+    if(names(temp.file2[,i3]) %in% lastname.cols){
+      #stop("ERROR!!!!!!!!!!!!!!!")
+      try(temp.file2[,i3] <- sample(x = lexicon::freq_last_names$Surname, size = nrow(temp.file2), replace = T, 
+                                   prob = lexicon::freq_last_names$prop))
+    }
+    # place dummy firstnames
+    if(names(temp.file2[,i3]) %in% firstmiddlename.cols){
+      try(temp.file2[,i3] <- sample(x = lexicon::freq_first_names$Name, size = nrow(temp.file2), replace = T, 
+                                   prob = lexicon::freq_first_names$prop))
+    }
+    # format phone number
+    if(names(temp.file2[,i3]) %in% phone.cols){
+      try(temp.file2[!unname(unlist(is.na(unlist(temp.file2[,i3])))),i3] <- 5555555555)
+    }
+    #format email address
+    if(names(temp.file2[,i3]) %in% email.cols){
+      try(temp.file2[!unname(unlist(is.na(unlist(temp.file2[,i3])))),i3] <- "fakename@fakeemail.com")
+    }
+    #format DOB
+    if(names(temp.file2[,i3]) %in% dob.cols){
+      try(temp.file2[!unname(unlist(is.na(unlist(temp.file2[,i3])))),i3] <- as_date(0))
+    }
+  }
+  try(temp.df2   <- temp.file2[sample(1:nrow(temp.file2), size = min(c(n_rows, nrow(temp.file2))), replace = F),])
+  try(new.files2[[i1]] <-  temp.df2)
+  rm(temp.file2)
+}
+
+# temp----
+# check that simulated data isn't real data
+
+getwd()
+rc <- read_csv("Client.csv")
+
+sc <- new.files[["Client.csv"]]
+
+
+
+pii_str <- function(pid,fn,mn,ln,ssn,dob){
+  return(paste(pid,fn,mn,ln,ssn,dob,sep=" "))
+}
+
+pii_str(sc$PersonalID,sc$FirstName, sc$MiddleName, sc$LastName, 
+        sc$SSN,sc$DOB) %in%
+pii_str(rc$PersonalID,rc$FirstName, rc$MiddleName, rc$LastName, 
+        rc$SSN,sc$DOB)
+
+# /temp----
+
+
+
+
+rm(list=ls()[!ls() %in% c("new.files", "new.files2", "output_files_dir", 
+                          "input_files_dir")])
+
+
+# write both datas to new files----
+# set dir to desired output location
+setwd(output_files_dir)
+
+names(new.files)
+for(i in names(new.files)){
+  new.name <- paste("RandRows_", i, sep = "", collapse = "")
+  write_csv(x = new.files[[i]], file = new.name)
+  rm(new.name)
+}
+
+names(new.files2)
+for(i in names(new.files2)){
+  new.name <- paste("RandObs_", i, sep = "", collapse = "")
+  write_csv(x = new.files2[[i]], file = new.name)
+  rm(new.name)
+}
+
+# compress files
+zip(zipfile = "simulated_RandRows.zip", 
+    files = c(list.files(pattern = "^RandRows.*\\.csv$")), 
+    recurse = F, 
+    mode = "cherry-pick")
+
+zip(zipfile = "simulated_RandObs.zip", 
+    files = c(list.files(pattern = "^RandObs.*\\.csv$")), 
+    recurse = F, 
+    mode = "cherry-pick")
+
+# remove generated zip files
+file.remove(list.files(pattern = "^RandRows.*\\.csv$"))
+file.remove(list.files(pattern = "^RandObs.*\\.csv$"))
+
+rm(new.files, new.files2, i, input_files_dir, output_files_dir)
+gc()
+
+
+
+

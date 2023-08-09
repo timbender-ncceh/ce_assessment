843e0c3a3d9cec204fed777f180850ed33717c4a
Tim Bender <105810134+timbender-ncceh@users.noreply.github.com>
2023-08-09 08:05:10 -0400

    Add files via upload

--- ce_assessment_micro.R
+++ ce_assessment_micro.R
@@ -7,6 +7,15 @@ library(forcats)
 library(openssl)
 library(glue)
 
+library(janitor)
+
+# functions to learn ----
+# select()
+# get()
+# unnest()
+# clean_names()
+# 
+
 setwd("C:/Users/TimBender/Documents/R/ncceh/projects/ce_assessment")
 
 rm(list=ls());cat('\f')#[!ls() %in% c("ce", 
---
+++ exploratory_data_analysis.R
@@ -0,0 +1,21 @@
+# exploratory data analysis----
+
+library(readr)
+library(dplyr)
+library(lubridate)
+library(data.table)
+library(ggplot2)
+library(forcats)
+library(openssl)
+library(glue)
+
+library(janitor)
+
+# functions to learn ----
+# select()
+# get()
+# unnest()
+# clean_names()
+# 
+
+setwd("C:/Users/TimBender/Documents/R/ncceh/projects/ce_assessment")
